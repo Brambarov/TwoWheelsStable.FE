993c7cf528547ba2530843fa5bf5d68a63a076e8
Brambarov <ivan.brambarov@gmail.com>
2024-12-09 19:23:33 +0200

    Create extractIdFromHref utility function

--- src/utils/String.tsx
+++ src/utils/String.tsx
@@ -2,3 +2,8 @@ export const toString = (value: unknown): string => {
   if (value === null || value === undefined) return "";
   else return String(value);
 };
+
+export const extractIdfromHref = (href: string): string => {
+  const parts = href.split("/");
+  return parts[parts.length - 1];
+};
