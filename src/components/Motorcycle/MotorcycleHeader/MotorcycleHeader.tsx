7ade662301ee0d30cea49bcb018b11c05956c824
Brambarov <ivan.brambarov@gmail.com>
2025-02-10 19:16:48 +0200

    Reformat the styling for MotorcycleHeader

--- src/components/Motorcycle/MotorcycleHeader/MotorcycleHeader.tsx
+++ src/components/Motorcycle/MotorcycleHeader/MotorcycleHeader.tsx
@@ -1,18 +1,21 @@
+import "./MotorcycleHeader.css";
+
 const MotorcycleHeader: React.FC<{
   motorcycle: any;
   images: string[];
 }> = ({ motorcycle, images }) => {
   return (
-    <div>
+    <div className="motorcycle-header">
       {images.length > 0 && (
-        <img src={images[0]} alt="Motorcycle" style={{ maxWidth: "100%" }} />
+        <img className="motorcycle-image" src={images[0]} alt="Motorcycle" />
       )}
-      <h1>{motorcycle.name}</h1>
-      <p>{motorcycle.make}</p>
-      <p>{motorcycle.model}</p>
-      <p>{motorcycle.year}</p>
-      <p>{motorcycle.mileage}</p>
-      <p>{motorcycle.owner}</p>
+      <div className="motorcycle-info">
+        <h1>{motorcycle.name}</h1>
+        <p>
+          {motorcycle.make} {motorcycle.model}, {motorcycle.year},{" "}
+          {motorcycle.mileage} km
+        </p>
+      </div>
     </div>
   );
 };
